5f6c7a8a390b010e4ae3a6472f782d869c652085
Simeon Valev <simeonvalev00@gmail.com>
2020-06-22 14:06:14 +0300

    Work on solution

--- Exams/01. Python Advanced Exam - 23 February 2020/02. Solutions/01. ExpressionEvaluator.py
+++ Exams/01. Python Advanced Exam - 23 February 2020/02. Solutions/01. ExpressionEvaluator.py
@@ -1,3 +1,5 @@
+import math
+
 inputItems = input().split(' ')
 sum = 0
 numbers = []
@@ -12,32 +14,47 @@ for item in inputItems:
                     sum += int(numbers[i]) * int(numbers[i + 1])
                     isFirst = False
                 else:
-                    sum *= int(numbers[i]) * int(numbers[i + 1])
+                    if len(numbers) == 1:
+                        sum *= int(numbers[i])
+                    else:
+                        sum *= int(numbers[i]) * int(numbers[i + 1])
             elif item == '/':
                 if isFirst:
                     sum += int(numbers[i]) / int(numbers[i + 1])
                     isFirst = False
                 else:
-                    sum /= int(numbers[i]) / int(numbers[i + 1])
+                    if len(numbers) == 1:
+                        sum /= int(numbers[i])
+                    else:
+                        sum = math.trunc(
+                            sum / int(numbers[i]) / int(numbers[i + 1]))
             elif item == '+':
                 if isFirst:
                     sum += int(numbers[i]) + int(numbers[i + 1])
                     isFirst = False
                 else:
-                    sum += int(numbers[i]) + int(numbers[i + 1])
+                    if len(numbers) == 1:
+                        sum += int(numbers[i])
+                    else:
+                        sum += int(numbers[i]) + int(numbers[i + 1])
             else:
                 if isFirst:
                     sum += int(numbers[i]) - int(numbers[i + 1])
                     isFirst = False
                 else:
-                    sum -= int(numbers[i]) - int(numbers[i + 1])
+                    if len(numbers) == 1:
+                        sum -= int(numbers[i])
+                    else:
+                        sum = sum - int(numbers[i]) - int(numbers[i + 1])
 
             del numbers[0]
-            del numbers[0]
+
+            if len(numbers) >= 1:
+                del numbers[0]
 
             if len(numbers) == 0:
                 break
     else:
         numbers.append(item)
 
-print(sum)
+print(math.trunc(sum))
